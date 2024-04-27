96f5ba15a498e5c504aa6d33cae10b970e30e6df
Xorboo <xorboo@gmail.com>
2024-04-27 16:16:21 +0200

    refactor into files

--- antichoker.ino
+++ antichoker.ino
@@ -1,65 +1,18 @@
-#define USE_LEDS
-
-#ifdef USE_LEDS
-#include <FastLED.h>
-#endif
-
-#include <Wire.h>
-#include "Adafruit_SGP40.h"
-
-#ifdef USE_LEDS
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-#define NUM_LEDS    17
-#define DATA_PIN    1
-#define BRIGHTNESS  128
-
-CRGB leds[NUM_LEDS];
-#endif
-
-Adafruit_SGP40 sgp;
+bool sgpInitialized = false;
+bool ledsInitialized = false;
 
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) { delay(10); } // Wait for serial console to open!
+  while (!Serial) { delay(10); }
 
-  Serial.println("Initializing SGP40...");
-
-  
-  if (!sgp.begin()){
-    Serial.println("Sensor not found :(");
-    while (1);
-  }
-  Serial.print("Found SGP40 serial #");
-  Serial.print(sgp.serialnumber[0], HEX);
-  Serial.print(sgp.serialnumber[1], HEX);
-  Serial.println(sgp.serialnumber[2], HEX);
-
-#ifdef USE_LEDS
-  Serial.println("Initializing LEDs...");
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, 700);
-  FastLED.clear();
-  FastLED.show();
-  Serial.println("LEDs initialized");
-#endif
+  sgpInitialized = setupSgp();
+  ledsInitialized = setupLeds();
 }
 
 int counter = 0;
 void loop() {
-#ifdef USE_LEDS
-  leds[0] = CRGB(255, 0, 0);
-  leds[1] = CRGB(0, 255, 0);
-  leds[2] = CRGB(0, 0, 255);
-  leds[3] = CRGB::White;
-  FastLED.setBrightness(BRIGHTNESS);
-  FastLED.show();
-#endif
-
-  int32_t raw = sgp.measureVocIndex();
-
-  Serial.print("Measurement: ");
-  Serial.println(raw);
+  int32_t airQuality = getAirQuality();
+  renderLeds();
   delay(1000);
 }
